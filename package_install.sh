e9fe6be8d3189d8add5c7a865ec672dbc8fac60e
Doug Walter <doug.walter@smoothstack.com>
2022-10-13 17:00:36 -0600

    added sudo, removed python 2 calls

--- package_install.sh
+++ package_install.sh
@@ -1,2 +1,2 @@
-pip3 install -r requirements.txt || pip install -r requirements.txt
-python setup.py install || python3 setup.py install
+sudo pip3 install -r requirements.txt
+python3 setup.py install
